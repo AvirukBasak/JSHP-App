3495bc5f5d389f67bbbeca094ef1c4c06ed079b5
OogleGlu <avirukbasak@gmail.com>
2022-01-13 14:09:03 +0530

    updated index.js to allow custom arguments

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 const JSHP = require('@oogleglu/jshp');
-JSHP.jshp('serve', `:${process.env.PORT}`, './Public');
+JSHP.jshp(process.argv[2] || 'serve', process.argv[3] || `:${process.env.PORT}`, process.argv[4] || './Public');
