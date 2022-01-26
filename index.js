714641c943e214ecbb327f03c726f27f31fe2537
Oogle Glu <avirukbasak@gmail.com>
2022-01-26 13:09:21 +0530

    Refractored code

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 const JSHP = require('@oogleglu/jshp');
-JSHP.jshp(process.argv[2] || 'serve', process.argv[3] || `:${process.env.PORT}`, process.argv[4] || './Public', process.argv[5]);
+JSHP.jshp(
+    process.argv[2] || 'serve',
+    process.argv[3] || `:${process.env.PORT}`,
+    process.argv[4] || './Public',
+    process.argv[5] || '--verbose'
+);
